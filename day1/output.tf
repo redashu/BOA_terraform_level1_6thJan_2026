38c3c266785c64b0785710a0790f4a40d02a0ff1
Ashutoshh <ashutoshh@linux.com>
2026-01-07 02:37:01 +0530

    updates

--- day1/output.tf
+++ day1/output.tf
@@ -1,9 +1,18 @@
 
 output "hello" {
-  value =   "Hello world from terraform"
+  value = "Hello world from terraform"
 }
 
-output              "hello1"    {
-  value =   "Hello world from terraform by ashutoshh"
+output "hello1" {
+  value = "Hello world from terraform by ashutoshh"
 
+}
+
+output "ashu-vm-publicIP" {
+  value = aws_instance.example.public_ip
+}
+
+output "ashu-privatip" {
+  value = aws_instance.example.private_ip
+  
 }
