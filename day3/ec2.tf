d3d6ffd4ef65a6e5290c9c5d54d0fefa72187792
Ashutoshh <ashutoshh@linux.com>
2026-01-09 01:01:17 +0530

    updates final

--- day3/ec2.tf
+++ day3/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "example" {
   ami           = var.ashu-ami
   instance_type = var.vm-size
+  count = var.novm
   # attaching security group to ec2 vm 
   vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
 
   tags = {
-    Name = var.vm-name
+    Name = "${var.vm-name}-${count.index}"
   }
 }
